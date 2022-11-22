638a8673fddaf7fac74baff7bce37c5453800024
dcode <dr.glasdou@gmail.com>
2022-11-22 09:10:06 -0600

    add: env variables

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,17 +1,12 @@
-import axios, { AxiosInstance } from 'axios';
-import { boot } from 'quasar/wrappers';
+import axios from 'axios';
 import useAuth from 'src/composables/useAuth';
 
 import { AuthResponse } from './../interfaces/auth.interface';
 
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-  }
-}
+console.log(import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: 'https://dcode-todo.herokuapp.com/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
@@ -41,17 +36,5 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
-export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
-  app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
-});
 
 export { api };
